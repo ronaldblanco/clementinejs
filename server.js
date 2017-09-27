6d1d10daba674dce87acc7f35652077389271c9c
Ronald Blanco <pnald.blanco@gmail.com>
2017-09-27 16:01:22 +0000

    added express.js file

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 'use strict';
 
-var express = require('express');
 var routes = require('./app/routes/index.js');
+var app = require('./express.js');
+
 var mongoose = require('mongoose');
 var passport = require('passport');
 var passportTwitter = require('passport');
 var passportLocal = require('passport');
-var session = require('express-session');
 
-var compression = require('compression');
 var winston = require('winston');
 require('winston-daily-rotate-file');
-var fs = require('fs');
 var functions = require('./app/common/functions.js');
-
-var app = express();
 
 require('dotenv').load();
 require('./app/config/passport')(passport);
@@ -25,7 +21,6 @@ mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
 /////EMAIL CONFIG////////////////////////////////////////////////////////////////////////////
-app.use('/emailjs', express.static(process.cwd() + '/node_modules/emailjs'));
 var emailServer = {
     'user' : process.env.EMAILUSER,
     'password' : process.env.EMAILPASS,
@@ -33,25 +28,6 @@ var emailServer = {
     'port' : process.env.EMAILPORT
 };
 ////////////////////////////////////////////////////////////////////////////////////
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-app.use('/socket', express.static(process.cwd() + '/node_modules/socket.io-client/dist'));
-
-//////////////////////////////////////////////
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-//////////////////////////////////////////////
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 //CHECK FOLDER LOG AND CREATE IT////////////////////////////////////
 functions.ensureExists(__dirname + '/log', '0744', function(err) {
@@ -69,25 +45,12 @@ var logger = new (winston.Logger)({
 functions.logIt(logger,'//////////////////STARTING LOGGER INFO////////////////////////');
 /////////////////////////////////////////////////
 
-//Forzing Cache of static/////////////////////////
-app.use(functions.cacheIt);
-/////////////////////////////////////////////////
-
-//COMPRESSION////////////////////////////////////
-app.use(compression({filter: functions.shouldCompress}));
-/////////////////////////////////////////////////
-
 routes(app, passport, passportTwitter, passportLocal, emailServer);
 
 //PORT config
 var port = process.env.PORT || 8080;
 
-//var config = require('./app/models/socketData.js');
 if (process.env.SOCKET === 'TRUE'){
-    //config.act = true;
-    //Instaling Socket.io and http
-    //functions.execute('npm install socket.io http');
-    
     //Uncomment to used the Websocket Controller
     //using: socket.io http and model config.js as test
     //WEBSOCKET///////////////////////////
@@ -95,7 +58,6 @@ if (process.env.SOCKET === 'TRUE'){
     var io = require('socket.io')(server);
     var webSocketHandler = require(process.cwd() + '/app/controllers/webSocketHandler.server.js');
 
-    var numClients = 0;
     var endpoint = io
         .of('/')
         .on('connection', function (socket) {
@@ -108,7 +70,6 @@ if (process.env.SOCKET === 'TRUE'){
     //WEBSOCKET//////////////////////////
     
 } else if (process.env.SOCKET === 'FALSE' || process.env.SOCKET === undefined || process.env.SOCKET === null){
-    //config.act = false;
     app.listen(port,  function () {
 	    console.log('Node.js listening on port ' + port + '...');
     });
